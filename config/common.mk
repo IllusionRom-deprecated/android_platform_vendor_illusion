9ca3c75c5f3739e64b785f6bd5f9b65d71865260
RobMacGregor <robmac8138@gmail.com>
2013-11-17 13:00:32 -0500

    Update common.mk
    edit omni vendor to illusion.

--- config/common.mk
+++ config/common.mk
@@ -2,7 +2,7 @@ PRODUCT_BRAND ?= omni
 
 # bootanimation
 PRODUCT_COPY_FILES += \
-	vendor/omni/bootanimation.zip:system/media/bootanimation.zip
+	vendor/illusion/bootanimation.zip:system/media/bootanimation.zip
 
 # general properties
 PRODUCT_PROPERTIES_OVERRIDE += \
@@ -22,33 +22,33 @@ endif
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/omni/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
-    vendor/omni/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
-    vendor/omni/prebuilt/bin/blacklist:system/addon.d/blacklist
+    vendor/illusion/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/illusion/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/illusion/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
+    vendor/illusion/prebuilt/bin/blacklist:system/addon.d/blacklist
 
 # init.d support
 PRODUCT_COPY_FILES += \
-	vendor/omni/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
-	vendor/omni/prebuilt/bin/sysinit:system/bin/sysinit
+	vendor/illusion/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
+	vendor/illusion/prebuilt/bin/sysinit:system/bin/sysinit
 
 # userinit support
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/etc/init.d/90userinit:system/etc/init.d/90userinit
+    vendor/illusion/prebuilt/etc/init.d/90userinit:system/etc/init.d/90userinit
 
 # Init script file with omni extras
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/etc/init.local.rc:root/init.omni.rc
+    vendor/illusion/prebuilt/etc/init.local.rc:root/init.omni.rc
 
 # Enable SIP and VoIP on all targets
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
 # Additional packages
--include vendor/omni/config/packages.mk
+-include vendor/illusion/config/packages.mk
 
 # Versioning
--include vendor/omni/config/version.mk
+-include vendor/illusion/config/version.mk
 
 # Add our overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/illusion/overlay/common

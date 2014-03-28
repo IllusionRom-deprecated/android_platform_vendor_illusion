68e5b74ad00d8e7481bea8adc42b471149a38a2f
AdityaSureka <adityasureka92@gmail.com>
2014-03-29 00:40:24 +0100

    Add note variants
    Change-Id: Idd3340624bee2196f92398d48cd6ff71ea259e21

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,8 @@ add_lunch_combo ill_d802-user
 add_lunch_combo ill_galaxysmtd-user
 add_lunch_combo ill_golden-user
 add_lunch_combo ill_hammerhead-user
+add_lunch_combo ill_i605-user
+add_lunch_combo ill_l900-user
 add_lunch_combo ill_i9300-user
 add_lunch_combo ill_i9305-user
 add_lunch_combo ill_ls980-user

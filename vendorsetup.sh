562cae2b56be4f50794b27af8112a1f95acbe33d
AdityaSureka <adityasureka92@gmail.com>
2014-03-28 13:59:02 -0400

    Derp
    Change-Id: I2d77641c07b40a739c2a8d973dfe0a697252f4e4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,14 +1,14 @@
-add lunch combo ill_d2lte-user
-add lunch combo ill_d800-user
-add lunch combo ill_d801-user
-add lunch combo ill_d802-user
-add lunch combo ill_galaxysmtd-user
-add lunch combo ill_golden-user
-add lunch combo ill_hammerhead-user
+add_lunch_combo ill_d2lte-user
+add_lunch_combo ill_d800-user
+add_lunch_combo ill_d801-user
+add_lunch_combo ill_d802-user
+add_lunch_combo ill_galaxysmtd-user
+add_lunch_combo ill_golden-user
+add_lunch_combo ill_hammerhead-user
 add_lunch_combo ill_i9300-user
 add_lunch_combo ill_i9305-user
-add lunch combo ill_ls980-user
+add_lunch_combo ill_ls980-user
 add_lunch_combo ill_n7100-user
 add_lunch_combo ill_t0lte-user
-add lunch combo ill_vs980-user
-add lunch combo ill_vigor-user
+add_lunch_combo ill_vs980-user
+add_lunch_combo ill_vigor-user

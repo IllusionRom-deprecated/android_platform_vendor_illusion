69679e42c0bf98aab074f83329d979b147e80abb
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2014-04-10 22:39:11 +0200

    Add Moto G and Galaxy Note user build
    Change-Id: I95f4ac493370f690003fe7d726e435ed188e17b4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@ add_lunch_combo ill_d2lte-user
 add_lunch_combo ill_d800-user
 add_lunch_combo ill_d801-user
 add_lunch_combo ill_d802-user
+add_lunch_combo ill_falcon-user
 add_lunch_combo ill_galaxysmtd-user
 add_lunch_combo ill_golden-user
 add_lunch_combo ill_hammerhead-user
@@ -11,6 +12,7 @@ add_lunch_combo ill_i9300-user
 add_lunch_combo ill_i9305-user
 add_lunch_combo ill_ls980-user
 add_lunch_combo ill_mako-user
+add_lunch_combo ill_n7000-user
 add_lunch_combo ill_n7100-user
 add_lunch_combo ill_t0lte-user
 add_lunch_combo ill_vs980-user

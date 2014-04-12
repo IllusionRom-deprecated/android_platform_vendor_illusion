786f81bb00590fbc634436b22502c66538d11448
Snuzzo <jrracinfan@gmail.com>
2014-04-12 12:42:43 -0400

    Add grouper to lunch combos
    Change-Id: I3f34574128cdf7c03c49333d094d30aae820fcbb

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,7 @@ add_lunch_combo ill_d802-user
 add_lunch_combo ill_falcon-user
 add_lunch_combo ill_galaxysmtd-user
 add_lunch_combo ill_golden-user
+add_lunch_combo ill_grouper-user
 add_lunch_combo ill_hammerhead-user
 add_lunch_combo ill_i605-user
 add_lunch_combo ill_l900-user
